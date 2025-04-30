fb574253addb853c75264b65f3d044138880ed64
snix422 <102601543+snix422@users.noreply.github.com>
2025-04-30 06:57:02 +0200

    add testing

---
+++ backend/FinanceAppWebApi/FinanceAppWebApi/Tests/Unit/ExpenseControllerUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using FinanceAppWebApi.Controllers;
+using FinanceAppWebApi.Services;
+using Moq;
+
+namespace FinanceAppWebApi.Tests.Unit
+{
+    public class ExpenseControllerUnitTests
+    {
+        private readonly Mock<ILogger<ExpenseController>> _logger;
+        private readonly Mock<IExpenseService> _expenseService;
+        private readonly ExpenseController _expenseController;
+
+        public ExpenseControllerUnitTests()
+        {
+            _logger = new Mock<ILogger<ExpenseController>>();
+            _expenseService = new Mock<IExpenseService>();
+            _expenseController = new ExpenseController(_expenseService.Object);
+        }
+    }
+}
